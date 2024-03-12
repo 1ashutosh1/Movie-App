8af91ee72113a540fc9333a3ffa1037f073b1645
1Ashutosh1 <at8969927@gmail.com>
2024-03-12 19:30:15 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="absolute w-full p-4 flex justify-between">
+      <Link to="/">
+        <h1 className="uppercase text-red-600 font-nsans-bold text-5xl">
+          Movix
+        </h1>
+      </Link>
+
+      <div>
+        <Link to="/login">
+          <button className="mr-4">Login</button>
+        </Link>
+
+        <Link to="/signup">
+          <button className="bg-red-600 px-4 py-2 rounded">Sign up</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
